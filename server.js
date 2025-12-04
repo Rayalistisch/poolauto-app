93caaebbcba374e7d8006a4bb7fc025cba09c4d3
Ray Huffenreuter <rhuffenreuter90@gmail.com>
2025-12-04 10:29:11 +0100

    Fix routing + persistente bookings

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // ---- BESTANDEN ----
 const DATA_FILE = path.join(__dirname, "bookings.json");
